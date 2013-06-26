24af9a6efcdf2a922209c1613f34c853b01ca6ea
ntimba20 <neal@m2automation.com>
2013-06-26 09:02:22 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lot::Application.config.secret_token = '2d6171bbbb891012146e23515f58be7a3b8bcc0273466e81600f086ac73ddbe09c4aa1fb48bab3532eb47bce569c29dd69ab8faa479d57b808ccc3b7ad231f0e'
